43491828e1c2c54450811d21783650ccf04bb941
K-Othman <karim_othman88@hotmail.com>
2024-06-13 00:35:14 +0100

    created the phone component and added the phone logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Check, Star } from "lucide-react";
-import Image from "next/image";
+import Phone from "@/components/Phone";
 
 export default function Home() {
   return (
@@ -84,6 +84,21 @@ export default function Home() {
                   </p>
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img
+                src="/your-image.png"
+                className="-top-20 select-none hidden sm:block lg:hidden xl:block"
+                alt=""
+              />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+                alt=""
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
