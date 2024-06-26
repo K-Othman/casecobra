2ffe89211da5f5509a272fdde1366adc4008c99c
K-Othman <karim_othman88@hotmail.com>
2024-06-26 14:58:38 +0100

    created the second review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -157,6 +157,40 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-2-none lg:grid-cols-2 gap-y-16">
+            <div className="flex gap-0.5 mb-2">
+              <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              <Star className="h-5 w-5 text-green-600 fill-green-600" />
+            </div>
+            <div className="text-lg leading-8">
+              <p>
+                &quot;I usually leave my phone in my pocket and that led to some
+                pretty heavy scratchmarks on all of my last phone cases. This
+                one, besides a barelt noticeable scratch on the corner,{" "}
+                <span className="p-0.5 bg-slate-800 text-white">
+                  looks brand new after about half a year
+                </span>
+                I dig it.&quot;
+              </p>
+            </div>
+            <div className="flex gap-4 mt-2">
+              <img
+                className="rounded-full h-12 w-12 object-cover"
+                src="/users/user-4.jpg"
+                alt="user"
+              />
+              <div className="flex flex-col">
+                <p className="font-semibold">Karim</p>
+                <div className="flex gap-1.5 items-center text-zinc-600">
+                  <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                  <p className="text-sm">Verified Purchase</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
