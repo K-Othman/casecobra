54cf042aead26f0af8b7f74e3e2cea5014c8f0db
K-Othman <karim_othman88@hotmail.com>
2024-06-26 14:51:50 +0100

    workingon testmonils section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,6 +131,31 @@ export default function Home() {
               <Star className="h-5 w-5 text-green-600 fill-green-600" />
               <Star className="h-5 w-5 text-green-600 fill-green-600" />
             </div>
+            <div className="text-lg leading-8">
+              <p>
+                &quot;The case feels durable and I even got a compliment on the
+                design. Had the case for two and a half months now and{" "}
+                <span className="p-0.5 bg-slate-800 text-white">
+                  the image is super clear
+                </span>
+                , on the case I had before, the image started fading into
+                yellow-ish color after a couple weeks. Love it.&quot;
+              </p>
+            </div>
+            <div className="flex gap-4 mt-2">
+              <img
+                className="rounded-full h-12 w-12 object-cover"
+                src="/users/user-1.png"
+                alt="user"
+              />
+              <div className="flex flex-col">
+                <p className="font-semibold">Jonathon</p>
+                <div className="flex gap-1.5 items-center text-zinc-600">
+                  <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                  <p className="text-sm">Verified Purchase</p>
+                </div>
+              </div>
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
