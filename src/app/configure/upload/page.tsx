b95ed1a3090b9be420cb73c48a3d8820f4dba9d3
K-Othman <karim_othman88@hotmail.com>
2024-07-26 14:45:33 +0100

    creating the drag and drop

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { useState } from "react";
+import Dropzone, { FileRejection } from "react-dropzone";
 
 const page = () => {
+  const [isDragOver, setIsDragOver] = useState<boolean>(false);
+
+  const onDropRejected = () => {};
+  const onDropAccepted = () => {};
+
   return (
     <div
       className={cn(
-        "relative h-full flex flex-1 my-16 w-full rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10"
+        "relative h-full flex flex-1 my-16 w-full rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl justify-center flex-col items-center",
+        {
+          "ring-blue-900/25 bg-blue-900/10": isDragOver,
+        }
       )}
-    ></div>
+    >
+      <div className="relative flex flex-1 lfex-col items-center w-full">
+        <Dropzone
+          onDropRejected={onDropRejected}
+          onDropAccepted={onDropAccepted}
+        ></Dropzone>
+      </div>
+    </div>
   );
 };
 
